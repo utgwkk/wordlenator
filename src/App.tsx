ea2015bbd7ca38d936733f7f980769cfc9ae2616
utgwkk <utagawakiki@gmail.com>
2022-02-10 20:19:02 +0900

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,65 @@
-import React from "react";
+import React, { CSSProperties, useMemo, useReducer } from "react";
+
+import styles from "./App.module.css";
 
 export function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <div className={styles.board}>
+      {Array.from("arisecough....................").map((ch, i) => (
+        <Character key={i} character={ch} />
+      ))}
+    </div>
+  );
+}
+
+type Status = "DEFAULT" | "NONE" | "HIT" | "BLOW";
+
+type CharacterProps = {
+  character: string;
+};
+
+function Character({ character }: CharacterProps) {
+  const [status, reduce] = useReducer((current: Status) => {
+    switch (current) {
+      case "DEFAULT":
+        return "HIT";
+      case "HIT":
+        return "BLOW";
+      case "BLOW":
+        return "NONE";
+      case "NONE":
+        return "DEFAULT";
+    }
+  }, "DEFAULT");
+
+  const style = useMemo<CSSProperties>(() => {
+    switch (status) {
+      case "NONE":
+        return {
+          color: "white",
+          backgroundColor: "#787c7e",
+        };
+      case "HIT":
+        return {
+          color: "white",
+          backgroundColor: "#6aaa64",
+        };
+      case "BLOW":
+        return {
+          color: "white",
+          backgroundColor: "#c9b458",
+        };
+      default:
+        return {
+          color: "black",
+          backgroundColor: "white",
+        };
+    }
+  }, [status]);
+
+  return (
+    <button className={styles.tile} onClick={reduce} style={style}>
+      {character}
+    </button>
+  );
 }
